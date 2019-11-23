83c6ee550e737bdc57734ce00d2281f85bcd546b
Pavan <abc@abc.com>
2019-11-23 23:57:48 +0530

    Added Employee Entity.

---
+++ src/main/java/com/example/demo/entity/Employee.java
@@ -0,0 +1,55 @@
+package com.example.demo.entity;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+
+@Entity
+public class Employee
+{
+    @Id
+    private String firstName;
+    private String lastName;
+    private char gender;
+    private String dob;
+    private String department;
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public char getGender() {
+        return gender;
+    }
+
+    public void setGender(char gender) {
+        this.gender = gender;
+    }
+
+    public String getDob() {
+        return dob;
+    }
+
+    public void setDob(String dob) {
+        this.dob = dob;
+    }
+
+    public String getDepartment() {
+        return department;
+    }
+
+    public void setDepartment(String department) {
+        this.department = department;
+    }
+}
